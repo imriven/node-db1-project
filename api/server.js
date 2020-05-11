3ab91ed5d5d1538d253ec1bb0459b764e5115436
Christina Harris <christinathedesigner@outlook.com>
2020-05-11 08:51:10 -0700

    okay false alarm now finished mvp working on stretch

--- api/server.js
+++ api/server.js
@@ -6,4 +6,34 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/accounts", (req, res)=> {
+    db("accounts")
+    .then(results => res.status(200).json(results))
+    .catch(err => res.status(500).send())
+}) 
+
+server.get("/accounts/:id", (req, res)=> {
+    db("accounts").where({ id: req.params.id })
+    .then(results => res.status(200).json(results))
+    .catch(err => res.status(500).send())
+})
+
+server.post("/accounts", (req, res)=> {
+    db("accounts").insert(req.body)
+    .then(results => res.status(201).send())
+    .catch(err => res.status(500).send())
+}) 
+
+server.put("/accounts/:id", (req, res)=> {
+    db("accounts").where({id: req.params.id})
+    .update(req.body)
+    .then(results => res.status(201).send())
+    .catch(err => res.status(500).send())
+}) 
+
+server.delete("/accounts/:id", (req, res)=> {
+    db("accounts").where({id: req.params.id}).del()
+    .then(results => res.status(204).send())
+    .catch(err => res.status(500).send())
+}) 
 module.exports = server;
