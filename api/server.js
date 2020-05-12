d155f04a1782555fe65d14fb16644d960ca736b9
christina smith <christina@christinaharris.design>
2020-05-12 12:14:09 -0700

    Christina harris (#1)
    * Completed project easy peasy
    
    * okay false alarm now finished mvp working on stretch
    
    * stretch complete
    
    * stretch error fix
    
    Co-authored-by: Christina Harris <christinathedesigner@outlook.com>

--- api/server.js
+++ api/server.js
@@ -6,4 +6,59 @@ const server = express();
 
 server.use(express.json());
 
+const middlewares = [checkQueryOrder]
+// server.get("/accounts", (req, res)=> {
+//     db("accounts")
+//     .then(results => res.status(200).json(results))
+//     .catch(err => res.status(500).send())
+// }) 
+
+server.get("/accounts/:id", (req, res)=> {
+    db("accounts").where({ id: req.params.id })
+    .then(results => res.status(200).json(results))
+    .catch(err => res.status(500).send())
+})
+
+server.post("/accounts", (req, res)=> {
+    db("accounts").insert(req.body)
+    .then(results => res.status(201).send())
+    .catch(err => res.status(500).send())
+}) 
+
+server.put("/accounts/:id", (req, res)=> {
+    db("accounts").where({id: req.params.id})
+    .update(req.body)
+    .then(results => res.status(201).send())
+    .catch(err => res.status(500).send())
+}) 
+
+server.delete("/accounts/:id", (req, res)=> {
+    db("accounts").where({id: req.params.id}).del()
+    .then(results => res.status(204).send())
+    .catch(err => res.status(500).send())
+}) 
+
+// Stretch
+
+server.get("/accounts", ...middlewares, (req, res)=> {
+     
+    db("accounts").where("budget", "<", req.query.max).orderBy("budget", req.query.order).limit(req.query.limit)
+    .then(results => {
+        res.status(200).json(results)
+       })
+    .catch(err => res.status(500).send())
+})
+
+function checkQueryOrder (req, res, next) {
+    const goodValues = ["desc", "asc"]
+    if (!goodValues.includes(req.query.order)) {
+        req.query.order = "desc"
+         } 
+         next();
+}
+
+
+
+//ocalhost:5000/accounts?limit=3&order=desc&max=5000
+
 module.exports = server;
--- queries.sql
+++ queries.sql
@@ -1,17 +1,28 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM Customers WHERE PostalCode=1010 ;
 
 -- Find the phone number for the supplier with the id 11
+SELECT Phone FROM [Suppliers]where SupplierID=11
 
 -- List first 10 orders placed, sorted descending by the order date
+SELECT * FROM [Orders] Order by OrderDate desc limit 10 
 
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT * FROM [Customers] where City= "London" or City ="Madrid" or Country="Brazil"SELECT * FROM [Customers] where City= "London" or City ="Madrid" or Country="Brazil"
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+insert into Customers (Country, CustomerName, ContactName, Address, City, PostalCode)
+  values ('Middle Earth', 'The Shire', 'Biblo Baggins', '1 Hobbit Hole', 'Bag End', '111');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+update Customers
+set City = 'Silicon Valley', Country = 'USA'
+where CustomerName = 'Lambda School'
 
--- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+-- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted //Added in the shire and got 70
+SELECT COUNT(DISTINCT City) FROM Customers;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT * FROM [Suppliers] where length(SupplierName) > 20
